d9cd6909b5a1a320fc27da525a3358095eb35a14
Rémy Beauperin <remy.beauperin@gmail.com>
2025-09-25 14:04:28 +0200

    Let's go

--- src/actions/products/actions/update.ts
+++ src/actions/products/actions/update.ts
@@ -8,7 +8,7 @@ import { prisma } from '@/lib/prisma';
 export const updateProduct = authActionClient
   .metadata({ actionName: "updateProduct" }) 
   .schema(z.object({...productModelUpdate, productId: z.string() }))
-  .action(async ({ parsedInput: { productId, ...parsedInput } }) => {
+  .action(async ({ parsedInput: { productId, ...parsedInput }, ctx: { user } }) => {
 
   try {
 
@@ -16,6 +16,20 @@ export const updateProduct = authActionClient
       where: { id: productId },
       data: parsedInput
     })
+
+    // If price is updated, also update the ProductOnSubCatalogue price
+    if (user?.user?.id) {
+      const test = await prisma.productOnSubCatalogue.updateMany({
+        where: {
+          productId: productId
+        },
+        data: {
+          price: parsedInput.price,
+        },
+      })
+
+      console.log(test)
+    }
 
     return { success: true, product: result };
   } catch (error) {
